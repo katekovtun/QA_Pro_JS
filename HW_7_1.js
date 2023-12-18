d844bf208c30c31f8cbd2c518f934f6e4d77bd5a
kovtun.kate <kovtun.kate@outlook.com>
2023-12-18 23:08:24 +0200

    HW 7

---
+++ HW_7_1.js
@@ -0,0 +1,9 @@
+//знайти принаймні шість символів і не містити літери А (великої або малої)
+
+var str = "I don't like tomatoes, because they smell bad.";
+var re = /^[^Aa]{6,}$/;
+//console.log(re.test(str));
+if (re.test(str)) {
+    console.log("Correct");
+} else {
+    console.log("Incorrect");
